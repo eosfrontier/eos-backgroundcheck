9aac1fb87e79a7604f19df8d01e6d9e10b11eba5
SulpherStær <s.g.delarie@gmail.com>
2022-05-25 23:26:54 +0200

    PWA Setup
    Setting all files for the PWA (not that it is ever needed, but it makes me personally quite happy)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,30 +6,6 @@
 	import Counter from '$lib/Counter.svelte';
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
 <style>
 	section {
 		display: flex;
@@ -58,3 +34,29 @@
 		display: block;
 	}
 </style>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		<div class="welcome">
+			<picture>
+				<source srcset="svelte-welcome.webp" type="image/webp" />
+				<img src="images/icons/backgroundcheck.svg" alt="Welcome" />
+			</picture>
+		</div>
+		to your new
+		<br />
+		SvelteKit app
+	</h1>
+
+	<h2>
+		try editing
+		<strong>src/routes/index.svelte</strong>
+	</h2>
+
+	<Counter />
+</section>
