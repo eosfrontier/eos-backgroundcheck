7cf38d758d104c28d783d843aee08955b4c7c08f
SulpherStær <s.g.delarie@gmail.com>
2022-05-31 23:37:09 +0200

    format & lint

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,10 +2,6 @@
 	export const prerender = true;
 </script>
 
-<script lang="ts">
-	import Counter from '$lib/Counter.svelte';
-</script>
-
 <svelte:head>
 	<title>Personnel Background Check</title>
 	<meta name="description" content="Svelte demo app" />
