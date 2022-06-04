5e971a79866128624fbb43b7266d9f7abe7ef46a
SulpherStær <s.g.delarie@gmail.com>
2022-06-04 23:39:28 +0200

    Start of background sheet
    The background sheet is now ready to be made

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,9 +2,13 @@
 	export const prerender = true;
 </script>
 
+<script lang="ts">
+	import BackGroundForm from './backgroundform/index.svelte';
+</script>
+
 <svelte:head>
 	<title>Personnel Background Check</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section />
+<BackGroundForm />
